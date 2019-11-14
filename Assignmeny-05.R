cac256407694237843445d3a790db2cff230ed5c
Emma Boratto <eeb323@lehigh.edi>
2019-11-14 18:19:04 -0500

    CSV to DF
    Assignment-05

---
+++ Assignmeny-05.R
@@ -0,0 +1,48 @@
+############################################################
+# Assignment 05 
+# Written by Emma Boratto                                  #
+############################################################
+
+ptm <- proc.time()
+DF <- read.csv("INDPRO.csv")
+CSV_READ_TIME <- (proc.time() - ptm)
+CSV_READ_TIME
+
+
+class(DF)
+typeof(DF)
+str(DF)
+
+
+
+if (!require("data.table")) install.packages("data.table")
+library("data.table")
+
+getDTthreads()
+getDTthreads(verbose=TRUE)
+setDTthreads(0)
+getDTthreads()
+
+
+ptm <- proc.time()
+DF <- fread("INDPRO.csv", header="auto", 
+            data.table=FALSE)
+FREAD_READ_TIME <- (proc.time() - ptm)
+FREAD_READ_TIME
+
+class(DF)
+typeof(DF)
+str(DF)
+names(DF)
+
+
+ptm <- proc.time()
+header <- read.table("INDPRO.csv", header = TRUE,
+                     sep=",", nrow = 1)
+DF <- fread("INDPRO.csv", skip=1, sep=",",
+            header=FALSE, data.table=FALSE)
+setnames(DF, colnames(header))
+rm(header)
+FREAD_READ_TIME <- (proc.time() - ptm)
+FREAD_READ_TIME
+############################################
